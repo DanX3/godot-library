ee8570c5077a5b3d5932c5d7a39e5befc2ee50be
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2023-10-02 15:23:02 +0200

    Added frame freeze to Utils

--- library/scripts/Utils.gd
+++ library/scripts/Utils.gd
@@ -1,8 +1,18 @@
 class_name Utils extends Node
 
-
+# returns a random direction vector
 static func rand_dir() -> Vector2:
 	return Vector2.RIGHT.rotated(randf_range(0.0, 2.0 * PI))
 
+## returns a random position inside the provided Rect2
 static func rand_rect2(rect: Rect2) -> Vector2:
 	return rect.position + rect.size * Vector2(randf_range(0.0, 1.0), randf_range(0.0, 1.0))
+
+
+func frame_freeze(time_scale: float, duration: float):
+	var start_scale: float = Engine.time_scale
+	Engine.time_scale = time_scale
+	var tween = get_tree().create_tween()
+	tween.tween_property(Engine, "time_scale", 1.0, 0.2 * duration).set_delay(0.8 * duration * time_scale)
+	await tween.finished
+	Engine.time_scale = start_scale
