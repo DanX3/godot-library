8367631c2cdedee5656da158ba94841afa5f6fc2
Daniele Tolomelli <optimans@mercury.wind3.hub>
2024-11-17 06:22:25 +0100

    Better camera shake

--- library/scripts/CameraShake.gd
+++ library/scripts/CameraShake.gd
@@ -1,17 +1,19 @@
 class_name CameraShake extends Node
 
-@export var scale:= 1.0
-@export var offset_y: Curve
-@export var duration_s:= 1.0
+@export var scale := 20.0
+@export var period := 0.01
+@export var duration_s := 1.0
 
 var parent_start_position: Vector2
+
 
 func shake():
 	parent_start_position = get_parent().position
 	var tween = get_tree().create_tween()
-	tween.tween_method(offset_parent, 0.0, 1.0, duration_s)
+	tween.tween_method(offset_parent, 1.0, 0.0, duration_s)
 	tween.finished.connect(func(): get_parent().position = parent_start_position)
 
+
 func offset_parent(t: float):
-	get_parent().position = parent_start_position \
-		+ scale * Vector2(0.0, offset_y.sample(t))
+	var offset = Vector2(0.0, t * scale * sin(t / period))
+	get_parent().position = parent_start_position + offset
