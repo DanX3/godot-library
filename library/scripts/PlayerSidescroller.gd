a2dfd5458e79583c60f3778c3bebc32d7e98c5a5
Daniele Tolomelli <optimans@mercury.wind3.hub>
2024-12-06 16:17:21 +0100

    Added multiple jumps to PlayerSideScroller

--- library/scripts/PlayerSidescroller.gd
+++ library/scripts/PlayerSidescroller.gd
@@ -6,6 +6,9 @@ extends CharacterBody2D
 ## for how many ms the player can still jump even not being on floor
 @export var is_on_floor_delay_ms := 100
 
+const MaxAirJumps = 1
+
+var air_jumps_left = MaxAirJumps
 var was_on_floor_ms = 0
 
 
@@ -16,9 +19,17 @@ func _physics_process(delta):
 	if is_on_floor():
 		y = 0.0
 		was_on_floor_ms = Time.get_ticks_msec()
+		air_jumps_left = MaxAirJumps
 	var was_on_floor = Time.get_ticks_msec() - was_on_floor_ms < is_on_floor_delay_ms
-	if Input.is_action_just_pressed("move_up") and was_on_floor:
-		y = -jump_force
+	if Input.is_action_just_pressed("move_up"):
+		if was_on_floor:
+			y = -jump_force
+		elif air_jumps_left > 0:
+			air_jumps_left -= 1
+			y = -jump_force
 	velocity = Vector2(move_toward(velocity.x, x * speed, 20.0), y)
-
 	move_and_slide()
+
+
+func is_moving():
+	return velocity.length_squared() > 0.0
