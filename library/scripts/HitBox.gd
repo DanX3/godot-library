d274fac3da958d54cb609ae805af5f796001f715
Daniele Tolomelli <optimans@mercury.wind3.hub>
2025-05-03 23:44:07 +0200

    Improved HitBox and HurtBox

--- library/scripts/HitBox.gd
+++ library/scripts/HitBox.gd
@@ -1,3 +1,21 @@
+## The hitbox works paired with the hurtbox
+## Set the damage of the hitbox to a reasonable value and correctly set the
+## collision mask/layer
 class_name HitBox extends Area2D
 
-@export var damage := 10
+signal hit(area: Area2D)
+
+@export var damage := -1
+
+## Optionally the hurtbox may filter by ID to prevent multiple collision with the same hitbox
+## Refresh the ID before the start of the attack to mark it as a different attack
+var id = -1
+
+func _ready() -> void:
+	monitoring = false
+
+func refresh_id():
+	var new_id = randi()
+	while new_id == id:
+		new_id = randi()
+	id = new_id
--- library/scripts/Hurtbox.gd
+++ library/scripts/Hurtbox.gd
@@ -1,23 +1,34 @@
+## The Hurtbox actively monitors for collision with the Hitbox\
+## Correctly setup the collision layer/mask
+## If Health is populates automatically communicates the damage
+## Optionally can filter for Hitboxes with different IDs to avoid being hit multiple
+## times by the same hitbox. The hitbox must refresh its ID to deal damage again
 class_name HurtBox extends Area2D
 
 signal hurt(damage)
 
 @export var health: Health
+@export var remembers_id = false
+var last_id = -1
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass  # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+func _ready() -> void:
+	monitorable = false
+	connect("area_entered", _on_area_entered)
 
 func _on_area_entered(area):
-	if area is HitBox:
-		var hitbox = area  # as HitBox
-		if health != null:
-			health.damage(hitbox.damage)
-		emit_signal("hurt", hitbox.damage)
+	# hit only with hitboxes
+	if not area is HitBox:
+		return
+	var hitbox = area as HitBox
+	
+	# skip collision if the last hitbox ID is the same as this one
+	if remembers_id:
+		if area.id == last_id:
+			return
+		last_id = area.id
+	
+	if health != null:
+		health.damage(hitbox.damage)
+	
+	hitbox.hit.emit()
+	hurt.emit(hitbox.damage)
