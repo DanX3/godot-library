238e255a2cadb335c972dabbb02a3a80d4531fbe
Daniele Tolomelli <optimans@mercury.wind3.hub>
2025-06-22 23:35:51 +0200

    Added warp effect

---
+++ library/scripts/impact.gd
@@ -0,0 +1,16 @@
+class_name ImpactEffect extends CanvasLayer
+
+@onready var texture = $Impact
+@onready var player = $AnimationPlayer
+
+func impact(world_pos: Vector2) -> void:
+	if player.is_playing():
+		player.stop()
+	var p = world_pos * get_viewport().canvas_transform.affine_inverse()
+	var viewport_coords = p / texture.get_viewport_rect().size
+	texture.material.set_shader_parameter("impact_position", viewport_coords)
+	player.play("impact")
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		impact(event.global_position)
